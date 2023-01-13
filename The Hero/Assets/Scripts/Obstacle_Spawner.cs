049d68b379965ad9268807f37322cb6d45258543
Mohadese Salehi <mohadesesalehiiii1380@gmail.com>
2023-01-13 13:00:10 +0330

    Show Random obstacle for boat
    use  Random,  Instantiate, yield return new WaitForSeconds

--- The Hero/Assets/Scripts/Obstacle_Spawner.cs
+++ The Hero/Assets/Scripts/Obstacle_Spawner.cs
@@ -7,12 +7,25 @@ public class Obstacle_Spawner : MonoBehaviour
     public GameObject[] obstacle;
     void Start()
     {
-        Instantiate(obstacle[0],transform.position,Quaternion.identity);
+       StartCoroutine(SpawnObstacles());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void obstacles (){
+        int rand = Random.Range(0,obstacle.Length);
+        Instantiate(obstacle[rand],transform.position,Quaternion.identity);
+
+    }
+    IEnumerator SpawnObstacles(){
+        while (true)
+        {
+              yield return new WaitForSeconds(4);
+        obstacles();
+        }
+      
     }
 }
