72574898ae4d34bd0ef4cbe2f303e2abf37e85d5
Mohadese Salehi <mohadesesalehiiii1380@gmail.com>
2023-01-14 19:06:10 +0330

    Add UI High and Last Score

--- The Hero/Assets/Scripts/Score_Manager.cs
+++ The Hero/Assets/Scripts/Score_Manager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- using UnityEngine.UI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
 
 public class Score_Manager : MonoBehaviour
 {
     // Start is called before the first frame update
     public int score= 0;
     public Text scoreText;
+      public Text HighScoreText;
+        public Text LastScoreText;
     public int highScore;
+    public static int lastScore = 0;
     void Start()
     {
+
      StartCoroutine(Score());  
-     highScore=0; 
+     StartCoroutine(Reload()); 
+     highScore= PlayerPrefs.GetInt("high_score",0);
+     HighScoreText.text="High Score:"+highScore.ToString();
+     LastScoreText.text="Last Score:"+lastScore.ToString();
     }
 
     // Update is called once per frame
@@ -21,7 +29,7 @@ public class Score_Manager : MonoBehaviour
        scoreText.text = score.ToString();
        if(score > highScore){
           highScore= score;
-          Debug.Log(highScore);
+          PlayerPrefs.SetInt("high_score",highScore);
        }
     }
     IEnumerator Score(){
@@ -29,7 +37,14 @@ public class Score_Manager : MonoBehaviour
        {
             yield return new WaitForSeconds(2);
          score = score + 1;
+         lastScore = score;
        // Debug.Log("Score :" + score);
        }
+    }
+    IEnumerator Reload(){
+       
+            yield return new WaitForSeconds(10);
+        SceneManager.LoadScene("Game");
+       
     }
 }
